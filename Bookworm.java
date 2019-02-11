c551e6fe2cb7310f8faf4c73a4a4765e3a0ef81c
jlargs64 <justinlargo7@gmail.com>
2019-02-10 22:25:09 -0500

    Added construction of input file

--- Bookworm.java
+++ Bookworm.java
@@ -1,14 +1,38 @@
+
 /**
  * @author Justin Largo
  * @version 2/22/19
  */
 
-public class Bookworm{
+import java.io.File;
+import java.util.Scanner;
 
-    public void findWord(){
+public class Bookworm {
 
-    }   
-    public static void main(String[] args){
+    public void findWord(String targetWord) {
 
+    }
+
+    public void findWord(String targetWord, String currentWord) {
+
+    }
+
+    public static void main(String[] args) {
+        
+        // Find and construct our test file and grid
+        File inputFile;
+        char[][] grid;
+        String fileName = args[0];
+        inputFile = new File(fileName);
+
+        // Construct the grid using inputFile
+        try {
+            Scanner scanner = new Scanner(inputFile);
+            while (scanner.hasNextLine()) {
+                String currentCol = scanner.nextLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
